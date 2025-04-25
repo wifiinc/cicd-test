c8b3751ad8140eab86867ae277bb318c0eeeb385
Daan Breur <daanbreur@gmail.com>
2025-04-25 22:40:42 +0200

    Update math.cpp

--- src/math.cpp
+++ src/math.cpp
@@ -1,9 +1,5 @@
 #include "math.h"
 
-int add(int a, int b) {
-    return a + b;
-}
+int add(int a, int b) { return a + b; }
 
-int subtract(int a, int b) {
-    return a - b;
-}
+int subtract(int a, int b) { return a - b; }
